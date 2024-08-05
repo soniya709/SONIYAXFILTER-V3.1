63c4eada84c746ae9d364a5b4f2285307cabedbe
soniya709 <155508996+soniya709@users.noreply.github.com>
2024-08-05 14:37:16 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,9 +22,8 @@ logging.getLogger("aiohttp.web").setLevel(logging.ERROR)
 
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
-from database.ia_filterdb import Media
-#from database.users_chats_db import db
 from database.ia_filterdb import Media, Media2, choose_mediaDB, db as clientDB
+from database.users_chats_db import db
 from info import *
 from utils import temp
 from typing import Union, Optional, AsyncGenerator
